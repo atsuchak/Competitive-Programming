5cf825a12393ffc9a54bf80a584008dc433ece00
atsuchak <ahnaftajwarsuchak@gmail.com>
2025-04-30 16:05:01 +0600

    add codeforces problem solution

---
+++ CodeForces/Problem Set/50A_DominoPiling.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	
+	int m, n;
+	cin >> m >> n;
+
+	int totalArea = m*n;
+
+	cout << totalArea/2 << endl;
+
+	return 0;
+}
