8e893718774ce301f5d3f70cc27df77de543c6fa
atsuchak <ahnaftajwarsuchak@gmail.com>
2025-04-30 12:16:39 +0600

    add hacker rank folder

---
+++ HackerRank/BasicDataTypes.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+
+	int a;
+	long long b;
+	char c;
+	float f;
+	double d;
+
+	scanf("%d %lld %c %f %lf", &a, &b, &c, &f, &d);
+
+	printf("%d\n%lld\n%c\n%.3f\n%lf", a, b, c, f, d);
+
+	return 0;
+}
