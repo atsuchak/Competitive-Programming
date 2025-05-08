891f379037c1a5b9953a5aea91e58d53d35e6d4a
atsuchak <suchak0630@gmail.com>
2025-05-09 01:29:39 +0600

    add leetcode Problems solution

--- LeetCode/Problem Set/1-TwoSum.cpp
+++ LeetCode/Problem Set/1-TwoSum.cpp
@@ -1,36 +1,13 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-#define ff first
-#define ss second
-#define all(x) (x).begin(), (x).end()
-#define sz(x) (int)(x).size()
-
-void solve() {
-    int num[] = {1, 2, 3, 4};
-    int target = 9;
-    int size = sizeof(num)/sizeof(num[0]);
-    bool found = false;
-
-    for(int i = 0; i < size; i++) {
-        for(int j = i+1; j < size; j++) {
-            if(num[i] + num[j] == target){
-                cout << "[" << i << ", " << j << "]" << endl;
-                found = true;
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        for(int i = 0; i < nums.size(); i++) {
+            for(int j = i + 1; j < nums.size(); j++) {
+                if(nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
             }
-            
         }
+        return {};
     }
-    if(!found) cout << "Not found";
-
-}
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    solve();
-
-    return 0;
-}
+};
