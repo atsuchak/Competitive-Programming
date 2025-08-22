05bc1daf3bf0e014e53587727a793bde1cf0285e
Ahnaf Tajwar Suchak <suchak0630@gmail.com>
2025-08-22 21:54:08 +0600

    Update Template.cpp

--- Documents/Template.cpp
+++ Documents/Template.cpp
@@ -4,18 +4,12 @@ using namespace std;
 #define ff first
 #define ss second
 #define all(x) (x).begin(), (x).end()
-#define sz(x) (int)(x).size()
-
-void solve() {
-    
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(nullptr);
 
-    solve();
+    
 
     return 0;
 }
