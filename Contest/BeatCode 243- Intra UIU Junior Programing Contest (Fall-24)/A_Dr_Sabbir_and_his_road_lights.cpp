0b0d0c565261743138ca914691fec903c20e4744
ahnaftajwarsuchak <ahnaftajwarsuchak@gmail.com>
2025-12-12 11:56:57 +0600

    solve beatcode 243 question

---
+++ Contest/BeatCode 243- Intra UIU Junior Programing Contest (Fall-24)/A_Dr_Sabbir_and_his_road_lights.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+
+using ll = long long;
+using vi = vector<int>;
+using vl = vector<ll>;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+
+void solve() {
+    ll x, n; cin >> x >> n;
+
+    vl a(n);
+    ll sum = 0;
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+        sum += a[i]*2;
+    }
+
+    sum >= x? cout << "YES" << endl : cout << "NO" << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+#ifndef ONLINE_JUDGE
+    freopen("D:/File/input.txt", "r", stdin);
+    freopen("D:/File/output.txt", "w", stdout);
+#endif
+
+    int t_case = 1;
+    // cin >> t_case;
+
+    while (t_case--) {
+        solve();
+    }
+
+    return 0;
+}
