0bb442cfbdba9971ecdc30dba2ded0860d1cb37c
atsuchak <suchak0630@gmail.com>
2026-03-13 01:26:28 +0600

    Add solution for Codeforces problem 732A; implement logic and input handling

---
+++ CodeForces/732A.cpp
@@ -0,0 +1,49 @@
+// ======================================================== //
+// ==      Logic works — until the judge executes       === //
+// ======================================================== //
+
+//Problem link: https://codeforces.com/problemset/problem/732/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define dbg(x) cerr << "["#x"]: " << x << endl
+
+typedef long long ll; typedef vector<int> vi; typedef vector<ll> vll;
+typedef vector<char> vc; typedef vector<string> vs;
+typedef pair<int, int> pii; typedef vector<pii> vpii;
+
+const int MOD = 1e9 + 7; const ll INF = 1e18;
+
+
+void solve() {
+    int a, b; cin >> a >> b;
+
+    int cnt = 1;
+    for(int i = 1; ; i++) {
+        if((a*i)%10 == b || (a*i)%10 == 0) break;
+        else cnt++;
+    }
+
+    cout << cnt << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    #ifndef ONLINE_JUDGE
+        freopen("D:/File/input.txt", "r", stdin); freopen("D:/File/output.txt", "w", stdout);
+    #endif
+
+    int t = 1;
+    // cin >> t;
+
+    for(int i = 0; i < t; i++) solve();
+
+    return 0;
+} 
+
+// coded by আহনাফ তাজওয়ার সুচক
