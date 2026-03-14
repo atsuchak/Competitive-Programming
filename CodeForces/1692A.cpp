b028e5869d58c6421ebbe387caf6581e779ff596
atsuchak <suchak0630@gmail.com>
2026-03-14 11:40:34 +0600

    Add solution for Codeforces problem 1692A; implement logic and input handling

---
+++ CodeForces/1692A.cpp
@@ -0,0 +1,49 @@
+// ======================================================== //
+// ==      Logic works — until the judge executes       === //
+// ======================================================== //
+
+//Problem link: https://codeforces.com/problemset/problem/1692/A
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define sz(x) (int)(x).size()
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define dbg(x) cerr << "["#x"]: " << x << endl
+
+typedef long long ll; typedef vector<int> vi; typedef vector<ll> vll;
+typedef vector<char> vc; typedef vector<string> vs;
+typedef pair<int, int> pii; typedef vector<pii> vpii;
+
+const int MOD = 1e9 + 7; const ll INF = 1e18;
+
+
+void solve() {
+    vi a(4); cin >> a[0]; 
+    
+    int cnt = 0;
+    for(int i = 1; i < 4; i++) {
+        cin >> a[i]; 
+        if(a[i] > a[0]) cnt++;
+    }
+
+    cout << cnt << endl;
+}
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+
+    #ifndef ONLINE_JUDGE
+        freopen("D:/File/input.txt", "r", stdin); freopen("D:/File/output.txt", "w", stdout);
+    #endif
+
+    int t = 1;
+    cin >> t;
+
+    for(int i = 0; i < t; i++) solve();
+
+    return 0;
+} 
+
+// coded by আহনাফ তাজওয়ার সুচক
